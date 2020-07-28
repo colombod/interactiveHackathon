35b711dc3f061b47061fcd2765f21e96374c5270
Steve Suh <suhsteve@gmail.com>
2020-07-27 22:08:36 -0700

    barcodelookup and spoonacular initial commit.

---
+++ src/BarcodeScanner/BarcodeLookup/BarcodeResponse.cs
@@ -0,0 +1,65 @@
+﻿namespace BarcodeScanner.BarcodeLookup
+{
+    public class BarcodeResponse
+    {
+        public Product[] products { get; set; }
+    }
+
+    public class Store
+    {
+        public string store_name { get; set; }
+        public string store_price { get; set; }
+        public string product_url { get; set; }
+        public string currency_code { get; set; }
+        public string currency_symbol { get; set; }
+    }
+
+    public class Review
+    {
+        public string name { get; set; }
+        public string rating { get; set; }
+        public string title { get; set; }
+        public string review { get; set; }
+        public string datetime { get; set; }
+    }
+
+    public class Product
+    {
+        public string barcode_number { get; set; }
+        public string barcode_type { get; set; }
+        public string barcode_formats { get; set; }
+        public string mpn { get; set; }
+        public string model { get; set; }
+        public string asin { get; set; }
+        public string product_name { get; set; }
+        public string title { get; set; }
+        public string category { get; set; }
+        public string manufacturer { get; set; }
+        public string brand { get; set; }
+        public string label { get; set; }
+        public string author { get; set; }
+        public string publisher { get; set; }
+        public string artist { get; set; }
+        public string actor { get; set; }
+        public string director { get; set; }
+        public string studio { get; set; }
+        public string genre { get; set; }
+        public string audience_rating { get; set; }
+        public string ingredients { get; set; }
+        public string nutrition_facts { get; set; }
+        public string color { get; set; }
+        public string format { get; set; }
+        public string package_quantity { get; set; }
+        public string size { get; set; }
+        public string length { get; set; }
+        public string width { get; set; }
+        public string height { get; set; }
+        public string weight { get; set; }
+        public string release_date { get; set; }
+        public string description { get; set; }
+        public string[] features { get; set; }
+        public string[] images { get; set; }
+        public Store[] stores { get; set; }
+        public Review[] reviews { get; set; }
+    }
+}
---
+++ src/BarcodeScanner/Classify.cs
@@ -0,0 +1,43 @@
+﻿
+public static class Classify
+{
+    public static int GetPerishibilityInDays(string aisleName) =>
+        aisleName switch
+        {
+            "Baking" => 365,
+            "Health Foods" => 60,
+            "Spices and Seasonings" => 365,
+            "Pasta and Rice" => 365,
+            "Bakery/Bread" => 7,
+            "Refrigerated" => 21,
+            "Canned and Jarred" => 365,
+            "Frozen" => 180,
+            "Nut butters, Jams, and Honey" => 180,
+            "Oil, Vinegar, Salad Dressing" => 90,
+            "Condiments" => 180,
+            "Savory Snacks" => 90,
+            "Milk, Eggs, Other Dairy" => 21,
+            "Ethnic Foods" => 21,
+            "Tea and Coffee" => 180,
+            "Meat" => 7,
+            "Gourmet" => 30,
+            "Sweet Snacks" => 365,
+            "Gluten Free" => 60,
+            "Alcoholic Beverages" => 800,
+            "Cereal" => 180,
+            "Nuts" => 180,
+            "Beverages" => 365,
+            "Produce" => 7,
+            "Not in Grocery Store/Homemade" => 7,
+            "Seafood" => 5,
+            "Cheese" => 21,
+            "Dried Fruits" => 90,
+            "Online" => 365,
+            "Grilling Supplies" => 365,
+            "Bread" => 7,
+            _ => 2
+        };
+
+
+
+}
