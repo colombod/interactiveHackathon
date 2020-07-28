2dbcd75a4123db90fe83ee98e5ce3e86eff81c98
Diego Colombo <dicolomb@microsoft.com>
2020-07-28 23:54:56 +0100

    updated

--- src/BarcodeScanner.Tests/DecoderTest.cs
+++ src/BarcodeScanner.Tests/DecoderTest.cs
@@ -8,21 +8,33 @@ namespace BarcodeScanner.Tests
 {
     public class DecoderTest
     {
-        [Fact]
-        public void can_decode_a_bitmap()
+        [Theory]
+        [InlineData("code_000.jpg", "712345678911")]
+        [InlineData("code_001.jpg", "753469010065")]
+        [InlineData("apples.png", "011110181770")]
+        [InlineData("bananas.png", "643126071631")]
+        [InlineData("ghirardelli.png", "747599414190")]
+        [InlineData("oranges.png", "840437100009")]
+        public void can_decode_a_bitmap(string imageSample, string expectedCode)
         {
-            var image = ImageHelper.LoadBitmap("code_000.jpg");
+            var image = ImageHelper.LoadBitmap(imageSample);
             var result = image.Decode();
-            result.Text.Should().Be("712345678911");
+            result.Text.Should().Be(expectedCode);
             
         }
 
-        [Fact]
-        public void can_decode_an_image()
+        [Theory]
+        [InlineData("code_000.jpg", "712345678911")]
+        [InlineData("code_001.jpg", "753469010065")]
+        [InlineData("apples.png", "011110181770")]
+        [InlineData("bananas.png", "643126071631")]
+        [InlineData("ghirardelli.png", "747599414190")]
+        [InlineData("oranges.png", "840437100009")]
+        public void can_decode_an_image(string imageSample, string expectedCode)
         {
-            var image = ImageHelper.LoadImage("code_000.jpg");
+            var image = ImageHelper.LoadImage(imageSample);
             var result = image.Decode();
-            result.Text.Should().Be("712345678911");
+            result.Text.Should().Be(expectedCode);
 
         }
     }
