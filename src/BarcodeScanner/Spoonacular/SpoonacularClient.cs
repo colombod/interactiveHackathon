e2558d4242c8a68a19d8e16ff05881ee9cbaa7b5
Steve Suh <suhsteve@gmail.com>
2020-07-28 18:20:34 -0700

    add classify api to spoonacular client

--- src/BarcodeScanner/Spoonacular/SpoonacularClient.cs
+++ src/BarcodeScanner/Spoonacular/SpoonacularClient.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace BarcodeScanner.Spoonacular
@@ -23,6 +25,28 @@ namespace BarcodeScanner.Spoonacular
         {
             var ingredientInformationJson = await _client.GetStringAsync($"food/ingredients/{(int)ingredient}/information?amount=1&apiKey={_apiKey}");
             return JsonConvert.DeserializeObject<IngredientInfo>(ingredientInformationJson);
+        }
+
+        public async Task<ClassifyResponse> Classify(string filePath)
+        {
+            var fileName = Path.GetFileName(filePath);
+            using var stream = new FileStream(filePath, FileMode.Open);
+            HttpContent content = new StreamContent(stream);
+
+            content.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+            {
+                Name = "file",
+                FileName = fileName
+            };
+
+            MultipartFormDataContent form = new MultipartFormDataContent
+            {
+                { content, fileName }
+            };
+
+            var response = await _client.PostAsync($"food/images/classify?apiKey={_apiKey}", form);
+            var contentJson = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<ClassifyResponse>(contentJson);
         }
     }
 }
