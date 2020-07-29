a6947bfa71921ff7cdd23ea41587be8dc803a177
Steve Suh <suhsteve@gmail.com>
2020-07-28 18:21:11 -0700

    add classify response class

---
+++ src/BarcodeScanner/Spoonacular/ClassifyResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarcodeScanner.Spoonacular
+{
+    public class ClassifyResponse
+    {
+        public string status { get; set; }
+        public string category { get; set; }
+        public double probability { get; set; }
+    }
+}
