d8d4c466c3cae3d99a4dd3735a1145980df03acf
Steve Suh <suhsteve@gmail.com>
2020-07-28 19:19:53 -0700

    write data file.

--- src/BarcodeScanner/Spoonacular/ClassifyResponse.cs
+++ src/BarcodeScanner/Spoonacular/ClassifyResponse.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BarcodeScanner.Spoonacular
+﻿namespace BarcodeScanner.Spoonacular
 {
     public class ClassifyResponse
     {
